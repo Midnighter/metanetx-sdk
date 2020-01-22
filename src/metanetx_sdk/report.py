2ccd61345de9726ce251d28ea821113f80c21d11
Midnighter <midnighter@posteo.net>
2020-01-22 15:00:47 +0100

    feat: add numerical and visual report functions

---
+++ src/metanetx_sdk/report.py
@@ -0,0 +1,160 @@
+# Copyright (c) 2019, Moritz E. Beber.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Provide reporting and plotting functions."""
+
+
+from typing import Literal
+
+import humanize
+import pandas as pd
+import plotly.graph_objects as go
+
+
+__all__ = (
+    "report_value_count",
+    "report_duplicates",
+    "plot_bincount",
+    "plot_frequency",
+)
+
+
+def report_value_count(data_frame: pd.DataFrame, column: str, digits: int = 2) -> str:
+    """
+    Report the number and percentage of non-empty values in the column.
+
+    Parameters
+    ----------
+    data_frame : pandas.DataFrame
+        A data frame with one or more columns.
+    column : str
+        The name of the column to report on.
+    digits : int, optional
+        The number of digits to report in the percentage (default 2).
+
+    Returns
+    -------
+    str
+        The number of non-empty cells and a percentage of the total number of rows.
+
+    """
+    count = data_frame[column].notnull().sum()
+    # The type of `count` is `numpy.int64` which when divided by zero yields `nan`.
+    # This is undesired and we rather raise an exception here.
+    if len(data_frame) == 0:
+        raise ZeroDivisionError("The data frame is empty!")
+    return f"{humanize.intcomma(count)} ({count / len(data_frame):.{digits}%})"
+
+
+def report_duplicates(data_frame: pd.DataFrame, column: str) -> pd.Series:
+    """
+    Report the number of times elements occur in a column.
+
+    Warnings
+    --------
+    Missing values in the column are ignored.
+
+    Parameters
+    ----------
+    data_frame : pandas.DataFrame
+        A data frame with one or more columns.
+    column : str
+        The name of the column to report on.
+
+    Returns
+    -------
+    pandas.Series
+        A series whose index contains the unique elements of the column and whose
+        values are counts of the number of times an element occurred.
+
+    """
+    return (
+        data_frame.loc[data_frame[column].notnull(), :]
+        .groupby(column, sort=False)
+        .size()
+    )
+
+
+def plot_frequency(
+    series: pd.Series,
+    xaxis_title: str,
+    yaxis_title: str = "Frequency",
+    yaxis_scale: Literal["log", "linear"] = "log",
+) -> go.Figure:
+    """
+    Compute a histogram of the given data points.
+
+    Parameters
+    ----------
+    series : pandas.Series
+        The data points to create a histogram of.
+    xaxis_title : str
+        The title for the horizontal plot axis.
+    yaxis_title : str, optional
+        The title for the vertial plot axis (default 'Frequency').
+    yaxis_scale : {'log', 'linear'}, optional
+        The vertical axis scale (default 'log').
+
+    Returns
+    -------
+    plotly.graph_objects.Figure
+        A simple histogram of the values as a bar plot.
+
+    """
+    fig = go.Figure()
+    fig.add_trace(go.Histogram(x=series,))
+    fig.update_layout(
+        xaxis_title_text=xaxis_title,
+        yaxis_title_text=yaxis_title,
+        yaxis_type=yaxis_scale,
+    )
+    return fig
+
+
+def plot_bincount(
+    series: pd.Series,
+    xaxis_title: str,
+    yaxis_title: str = "Frequency",
+    yaxis_scale: Literal["log", "linear"] = "log",
+) -> go.Figure:
+    """
+    Generate a bar plot of the given values. Uses the index as tick labels.
+
+    Parameters
+    ----------
+    series : pandas.Series
+        The data points to create a histogram of.
+    xaxis_title : str
+        The title for the horizontal plot axis.
+    yaxis_title : str, optional
+        The title for the vertial plot axis (default 'Frequency').
+    yaxis_scale : {'log', 'linear'}, optional
+        The vertical axis scale (default 'log').
+
+    Returns
+    -------
+    plotly.graph_objects.Figure
+        A simple bar plot of the passed values. Uses the index of the series as tick
+        labels on the horizontal axis.
+
+    """
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x=series.index, y=series,))
+    fig.update_layout(
+        xaxis_title_text=xaxis_title,
+        yaxis_title_text=yaxis_title,
+        yaxis_type=yaxis_scale,
+    )
+    return fig
---
+++ tests/test_unit/test_report.py
@@ -0,0 +1,83 @@
+# Copyright (c) 2019, Moritz E. Beber.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Ensure the expected outcomes of reporting functions."""
+
+
+from string import ascii_lowercase
+
+import pytest
+from numpy import linspace
+from pandas import DataFrame, Series
+
+import metanetx_sdk.report as report
+
+
+LETTERS = sorted(ascii_lowercase)[:5]
+
+
+@pytest.mark.parametrize(
+    "table, column, expected",
+    [
+        pytest.param(
+            DataFrame({"z": []}),
+            "foo",
+            None,
+            marks=pytest.mark.raises(exception=KeyError),
+        ),
+        pytest.param(
+            DataFrame({"z": []}),
+            "z",
+            None,
+            marks=pytest.mark.raises(exception=ZeroDivisionError),
+        ),
+        (DataFrame({"a": range(5)}), "a", "5 (100.00%)"),
+        (DataFrame({"b": linspace(0.0, 1.0, 5)}), "b", "5 (100.00%)"),
+        (DataFrame({"c": LETTERS}), "c", "5 (100.00%)"),
+        (DataFrame({"d": [1, 1.5, None, "foo", "bar"]}), "d", "4 (80.00%)"),
+        (DataFrame({"e": [None] * 5}), "e", "0 (0.00%)"),
+    ],
+)
+def test_report_value_count(table: DataFrame, column: str, expected: str):
+    """Expect the value counts to be accurate."""
+    assert report.report_value_count(table, column) == expected
+
+
+@pytest.mark.parametrize(
+    "table, column, expected",
+    [
+        pytest.param(
+            DataFrame({"z": []}),
+            "foo",
+            None,
+            marks=pytest.mark.raises(exception=KeyError),
+        ),
+        (DataFrame({"a": []}), "a", Series([], dtype=int)),
+        (
+            DataFrame({"b": LETTERS + LETTERS}),
+            "b",
+            Series([2] * 5, index=LETTERS, dtype=int),
+        ),
+        (
+            DataFrame({"c": ["foo", None, "bar"]}),
+            "c",
+            Series([1] * 2, index=["foo", "bar"], dtype=int),
+        ),
+    ],
+)
+def test_report_duplicates(table: DataFrame, column: str, expected: Series):
+    """Expect the value counts to be accurate."""
+    assert (report.report_duplicates(table, column).index == expected.index).all()
+    assert (report.report_duplicates(table, column).values == expected.values).all()
